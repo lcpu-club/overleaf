d9ebb75f9df65a9c9f833722e5bacf33435e4c23
thezzisu <thezzisu@gmail.com>
2024-10-05 00:07:55 +0800

    fix: iaaa csp

--- lcpu/js/iaaa.js
+++ lcpu/js/iaaa.js
@@ -1,22 +1,28 @@
 function doIAAA() {
-    document.write(
-      "<form action='https://iaaa.pku.edu.cn/iaaa/oauth.jsp' method=post name='formx1' style='display:none'>"
-    );
-    document.write("<input type=hidden name='appID' value='latex_online'>");
-    document.write(
-      "<input type=hidden name='appName' value='北京大学LaTeX公共平台'>"
-    );
-    document.write(
-      "<input type=hidden name='redirectUrl' value='https://latex.pku.edu.cn/auth/iaaa'>"
-    );
-    document.write("</form>");
-    setTimeout("document.formx1.submit();", 1000);
-  }
-  
-  if (location.pathname === '/user/settings') {
-    window.addEventListener('load', (event) => {
-      const ele = document.getElementById('email-input')
-      if (ele && !ele.disabled) { ele.disabled = true }
-    })
-  }
-  
+  document.write(
+    "<form action='https://iaaa.pku.edu.cn/iaaa/oauth.jsp' method=post name='formx1' style='display:none'>"
+  );
+  document.write("<input type=hidden name='appID' value='latex_online'>");
+  document.write(
+    "<input type=hidden name='appName' value='北京大学LaTeX公共平台'>"
+  );
+  document.write(
+    "<input type=hidden name='redirectUrl' value='https://latex.pku.edu.cn/auth/iaaa'>"
+  );
+  document.write("</form>");
+  setTimeout("document.formx1.submit();", 1000);
+}
+
+if (location.pathname === '/user/settings') {
+  window.addEventListener('load', (event) => {
+    const ele = document.getElementById('email-input')
+    if (ele && !ele.disabled) { ele.disabled = true }
+  })
+}
+
+if(location.pathname === '/login'){
+  window.addEventListener('load', (event) => {
+    const ele = document.getElementById('iaaa-submit')
+    ele?.addEventListener('click', doIAAA)
+  })
+}
