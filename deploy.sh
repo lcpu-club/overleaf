f4cd26809e3367d1d7c84e8db36804f7aa8e8c62
leavelet <leavelet@163.com>
2024-10-06 20:11:15 +0800

    feat: update deploy code

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 
 source .env
 
-sed -i "s/REPLACE_ME_IAAA_KEY/$REPLACE_ME_IAAA_KEY/g" services/web/modules/launchpad/app/src/LaunchpadRouter.js
-sed -i "s/REPLACE_ME_IAAA_ENDPOINT/$REPLACE_ME_IAAA_ENDPOINT/g" services/web/modules/launchpad/app/src/LaunchpadRouter.js
+sed -i "s|REPLACE_ME_IAAA_KEY|$REPLACE_ME_IAAA_KEY|g" services/web/modules/launchpad/app/src/LaunchpadRouter.js
+# replace REPLACE_ME_IAAA_ENDPOINT with real endpoint, mind the slashes
+sed -i "s|REPLACE_ME_IAAA_ENDPOINT|$REPLACE_ME_IAAA_ENDPOINT|g" services/web/modules/launchpad/app/src/LaunchpadRouter.js
 
 # download fonts from lcpu jfrog
 curl -H "Authorization: Bearer $JFROG_TOKEN" "https://jfrog-internal.lcpu.dev/artifactory/latex/fonts.zip"
