fc8d91d65c490168c12c0bcd1a633d9f8fe1f8a8
HGChuang <2401833@stu.neu.edu.cn>
2025-10-26 20:13:39 +0800

    runit

--- services/llm/app.js
+++ services/llm/app.js
@@ -4,7 +4,7 @@ import connectDB from './config/db.js';
 import keysRoutes from './app/routes/keys.routes.js';
 import llmRoutes from './app/routes/llm.routes.js';
 import cookieParser from 'cookie-parser';
-import { PORT,LISTEN_ADDRESS}  from './config/settings.defaults.cjs';
+import settings from '@overleaf/settings'
 
 const app = express();
 
@@ -20,8 +20,8 @@ app.use('/api/v1/llm', keysRoutes);
 
 app.use('/api/v1/llm', llmRoutes);
 
-app.listen(PORT, LISTEN_ADDRESS, () => {
-  console.log(`server running on ${PORT}`);
+app.listen(settings.PORT, settings.LISTEN_ADDRESS, () => {
+  console.log(`server running on ${settings.PORT}`);
 });
 
 
